5fb600b6c1b57c59f2c290e245912c6157cc697c
Mathieu Grasland <mathieu.grasland.ext@lvmh.com>
2025-08-27 11:02:15 +0200

    refacto: optim

---
+++ public/firebase-config.js
@@ -0,0 +1,18 @@
+// Configuration Firebase centralisée
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBs1O6cYYxNLE2vB9gEcmhEhONrditDyCo",
+    authDomain: "on-va-ou-470217.firebaseapp.com",
+    projectId: "on-va-ou-470217",
+    storageBucket: "on-va-ou-470217.firebasestorage.app",
+    messagingSenderId: "687464295451",
+    appId: "1:687464295451:web:b228ea2a0ddc668e5f1cbe",
+    measurementId: "G-2PCHXTT6DW"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
